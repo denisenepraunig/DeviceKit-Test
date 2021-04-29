7bef57bdf4bf44133598fd3e1c56682961d5ec42
Denise Nepraunig <denise.nepraunig@gmail.com>
2021-04-29 23:07:16 +0200

    inital project

---
+++ DeviceKit-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DeviceKit-Test
+//
+//  Created by Denise Nepraunig on 29.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DeviceKit-Test/DeviceKit_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceKit_TestApp.swift
+//  DeviceKit-Test
+//
+//  Created by Denise Nepraunig on 29.04.21.
+//
+
+import SwiftUI
+
+@main
+struct DeviceKit_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
