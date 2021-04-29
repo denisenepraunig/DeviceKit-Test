b4d8e6645a556cabe2a09999eeb8578c38aecfb8
Denise Nepraunig <denise.nepraunig@gmail.com>
2021-04-29 23:21:09 +0200

    first test

--- DeviceKit-TestTests/DeviceKit_TestTests.swift
+++ DeviceKit-TestTests/DeviceKit_TestTests.swift
@@ -6,28 +6,14 @@
 //
 
 import XCTest
+import DeviceKit
 @testable import DeviceKit_Test
 
 class DeviceKit_TestTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    let device = Device.current
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testDeviceSimulator() {
+      XCTAssertTrue(device.isOneOf(Device.allSimulators))
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
